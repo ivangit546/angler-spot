00726930e3ae022f7dbf08ca3a4b165a4c80ed65
ivanjaviersosa <javiers3@uwm.edu>
2026-05-01 18:49:28 -0500

    override save post test rotate3

--- angler/models/post.py
+++ angler/models/post.py
@@ -27,7 +27,7 @@ class Post(models.Model):
             response = requests.get(self.image.url)
             if response.status_code == 200:
                 image = Image.open(BytesIO(response.content))
-                print(image._getexif().get(274) if image._getexif() else "No EXIF")
+                image.info.pop('exif', None)
                 image = ImageOps.exif_transpose(image)
                 output = BytesIO()
                 image.save(output, format=image.format or 'JPEG')
