1614ca050079eacffe7f99e2cdc7571ff90fdc85
ivanjaviersosa <javiers3@uwm.edu>
2026-05-10 23:17:51 -0500

    create groupchat default name fix3

--- angler/views/direct_message.py
+++ angler/views/direct_message.py
@@ -28,7 +28,7 @@ class CreateGroupChat(LoginRequiredMixin, View):
         else:    
             group_chat = GroupChat.objects.create(owner=request.user)
             group_chat.set_default_image()
-            group_chat.group_name = group_chat.owner.get_profile_image
+            group_chat.group_name = group_chat.owner.get_profile_name
             group_chat.save()
             group_chat.users.add(user_to_message)
         context = {
