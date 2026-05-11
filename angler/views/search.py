6448a7984e81e3a9bb0e5edee817527a845e2656
ivanjaviersosa <javiers3@uwm.edu>
2026-05-10 22:44:29 -0500

    search posts query fix

--- angler/views/search.py
+++ angler/views/search.py
@@ -2,16 +2,18 @@ from django.shortcuts import render
 from django.views import View
 from angler.models.post import Post, Like
 from django.contrib.postgres.search import SearchQuery, SearchVector, SearchHeadline
+from django.db.models import Q
 
 class SearchView(View): 
     def post(self, request):
         searched = request.POST.get('searched')
+        friendslist = request.user.friends
         if searched:
             vector = SearchVector('text', 'user__username', 'user__profile__profile_name')
             query = SearchQuery(searched)
             posts = Post.objects.annotate(search=vector).filter(search=query).annotate(headline_text=SearchHeadline('text', query, start_sel="<mark>", stop_sel="</mark>", highlight_all=True),
                                                                                            headline_username=SearchHeadline('user__username', query, start_sel="<mark>", stop_sel="</mark>", highlight_all=True),
-                                                                                               headline_profile_name=SearchHeadline('user__profile__profile_name', query, start_sel="<mark>", stop_sel="</mark>", highlight_all=True))
+                                                                                               headline_profile_name=SearchHeadline('user__profile__profile_name', query, start_sel="<mark>", stop_sel="</mark>", highlight_all=True)).exclude(is_private=True & ~Q(user__in=friendslist))
         else:   
             posts = None    
         liked_post_ids = set(Like.objects.filter(user=request.user).values_list('post_id', flat=True))
